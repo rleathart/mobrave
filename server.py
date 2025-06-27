5e8938bcc311a5f73d9a518d23353d1877976c90
Robin Leathart <56587304+rleathart@users.noreply.github.com>
2025-06-27 10:33:50 +0100

    Update server.py headers
    Added a --debug flag which controls the serving of the CORS headers. The
    only reason I have the Sec-Fetch-Mode check in there is because the
    Chrome C++ DevTools extension doesn't work unless I have that.
    
    I think conditionally serving the CORS headers was causing some issues
    when serving to actual clients so I've gated it behind a --debug flag.
    
    Also added some more aggressive anti-caching measures for the executor.*
    files to hopefully solve that issue where sometimes the WASM module just
    doesn't instantiate unless you open the inspector.

--- server.py
+++ server.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 import ssl;
+import argparse
 from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
+
+def should_send_cors_headers(req_headers):
+    if args.debug:
+        return req_headers['Sec-Fetch-Mode'] != 'no-cors';
+
+    return True
 
 class Handler(SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
@@ -10,8 +17,13 @@ class Handler(SimpleHTTPRequestHandler):
 class CORSServer(Handler):
     def end_headers(self):
         self.send_header("Accept-Ranges", "bytes")
-        self.send_header("Access-Control-Allow-Origin", "*")
-        if not self.headers['Sec-Fetch-Mode'] == 'no-cors':
+
+        if 'executor' in self.path:
+            self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
+            self.send_header("Pragma", "no-cache")
+            self.send_header("Expires", "0")
+
+        if should_send_cors_headers(self.headers):
             self.send_header("Cross-Origin-Opener-Policy", "same-origin")
             self.send_header("Cross-Origin-Embedder-Policy", "require-corp")
             self.send_header("Cross-Origin-Resource-Policy", "same-origin")
@@ -19,6 +31,11 @@ class CORSServer(Handler):
         super().end_headers()
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser();
+    parser.add_argument('--debug', action='store_true')
+    args = parser.parse_args();
+
     port = 8080
     server_address = ("", port)  # Serve on all available interfaces
     httpd = ThreadingHTTPServer(server_address, CORSServer)
